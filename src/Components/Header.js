2e8388ef7ba761ae43ca348bc711c3609c3ff049
AdarshMitr <adarshmitr@gmail.com>
2022-10-02 12:15:21 +0530

    added responsiveness

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,13 +7,13 @@ const Header=()=>{
           <nav className="navbar navbar-expand-lg ">
             <div className="container-fluid">
               <a className="navbar-brand" href="#">YOUR LOGO</a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
+              <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul className="navbar-nav d-flex ">
                   <li className="nav-item">
-                    <a className="nav-link " aria-current="page" href="#">HOME</a>
+                    <a className="nav-link active" aria-current="page" href="#">HOME</a>
                   </li>
                   <li className="nav-item ">
                     <a className="nav-link" href="#">ABOUT</a>
